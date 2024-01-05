30f02e8674ca42382fdb52571d784c740ed65344
drevinskiyvlad <114866402+drevinskiyvlad@users.noreply.github.com>
2024-01-05 00:43:18 +0000

    Deploying to gh-pages from @ drevinskiyvlad/Task_9_Playwright_Pytest@6be64425605f1946388b43e510b0c3d3de1464f9 🚀

--- data/data.py
+++
@@ -1,4 +0,0 @@
-class Data:
-    countries = ["India", "United States", "Canada", "Australia", "Israel", "New Zealand", "Singapore"]
-    valid_email = "john.doe124@example.com"
-    valid_password = "1234"
